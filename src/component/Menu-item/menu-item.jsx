d4832613b339a93c048d42edb6e1d70f2c37f1ca
Tripleoniks <84743221+Tripleoniks@users.noreply.github.com>
2021-12-09 22:50:08 +0100

    broken component into folders

---
+++ src/component/Menu-item/menu-item.jsx
@@ -0,0 +1,15 @@
+import './menu-item.scss';
+
+const MenuItem = ({title, imageUrl, size}) => {
+    return ( 
+        <div className= {`${size} menu-item`}>
+            <div style={{ backgroundImage : `url(${imageUrl})`}} className='background-image' />
+        <div className='content'>
+            <h1 className='title'>{title.toUpperCase()}</h1>
+            <span className='subtitle'>SHOP NOW</span>
+        </div>
+    </div>
+     );
+}
+ 
+export default MenuItem;
---
+++ src/pages/Homepage/Homepage.jsx
@@ -0,0 +1,12 @@
+import './Homepage.scss';
+import Directory from '../../component/directory/directory';
+
+const HomePage = () => {
+    return ( 
+        <div className='homepage'>
+            <Directory/>
+        </div>
+     );
+}
+ 
+export default HomePage;
