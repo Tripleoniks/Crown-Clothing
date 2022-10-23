a461ca9713c95f443fd812251b8f3e4752f10e4e
Michael Akinsanya <tripleoniks9@gmail.com>
2022-10-23 21:56:21 +0100

    Update form-input.jsx

--- src/component/form-input/form-input.jsx
+++ src/component/form-input/form-input.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import './form-input.scss';
-
 const FormInput = ({ handleChange, label, ...otherProps }) => (
   <div className='group'>
     <input className='form-input' onChange={handleChange} {...otherProps} />
